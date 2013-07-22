0fd87bad409afc4b5c597d3a7d1e0d28be2bd1a0
Jacob Farkas <jacobf@rkas.net>
2013-07-21 23:01:07 -0700

    Adding an admin UI

--- summary/urls.py
+++ summary/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('summary.views',
     (r'^$', 'index'),
     (r'^new/$', 'newreply'),
-    (r'^(?P<reply_uuid>.*)/update/$', 'updatereply'),
+    (r'^createupdate/$', 'createupdate'),
 )
--- summary/views.py
+++ summary/views.py
@@ -1,15 +1,47 @@
+from django.template import Context, loader, RequestContext
+from django.shortcuts import render_to_response
+from django.http import HttpResponse, HttpResponseRedirect
 from reply.models import Person, Reply, FailedAttempt
 
 def index(request):
-    return render_to_response('summary.html', {}, context_instance=RequestContext(request))
+    return render_to_response('summary.html', {
+        'replyYes' : Reply.objects.filter(attending=True),
+        'replyNo'  : Reply.objects.filter(attending=False, replyDate__isnull=False),
+        'replyNone' : Reply.objects.filter(replyDate__isnull=True),
+    }, context_instance=RequestContext(request))
     
 def newreply(request):
     return render_to_response('adminNewReply.html', {
-        "person" : p,
-        "reply"  : r,
-        "plusOnePerson" : plusOnePerson,
-        "greeting" : ["Hello", "Hi", "Howdy", "Welcome"],
+        'numPeople' : xrange(6)
     }, context_instance=RequestContext(request))
     
-def updatereply(request):
-    return index(request)
+def createupdate(request):
+    allPeople = []
+    for i in xrange(6):
+        firstName = request.POST.get('firstName' + str(i))
+        if firstName is not None and len(firstName) > 0:
+            lastName = request.POST.get('lastName' + str(i)) or ""
+            nickName = request.POST.get('nickName' + str(i)) or ""
+            preferNick = request.POST.get('preferNick' + str(i)) or False
+            allPeople.append(Person(firstName=firstName, 
+                       lastName=lastName, 
+                       nickName=nickName,
+                       prefersNickName=preferNick))
+    
+    r = None
+    if len(allPeople) > 0:
+        r = Reply()
+        r.ip="127.0.0.1"
+        r.save()
+        for p in allPeople:
+            p.save()
+            r.invitedPeople.add(p)
+            r.attendingPeople.add(p)
+        r.email = request.POST.get('email')
+        r.hasPlusOne = request.POST.get('allowPlusOne') or False
+        r.save()
+    
+    return render_to_response('adminNewReply.html', {
+        'numPeople' : xrange(6),
+        'lastReply' : r,
+    }, context_instance=RequestContext(request))
