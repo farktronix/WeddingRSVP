cece49d232a9a84daba2524cb6d1a27d2aedc548
Jacob Farkas <jacobf@rkas.net>
2013-07-30 02:50:55 -0700

    Adding an email update page

--- summary/urls.py
+++ summary/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('summary.views',
     (r'^$', 'index'),
     (r'^new/$', 'newreply'),
+    (r'^emails/$', 'emails'),
+    (r'^updateemails/$', 'updateemails'),
     (r'^createupdate/$', 'createupdate'),
 )
--- summary/views.py
+++ summary/views.py
@@ -1,7 +1,7 @@
 from django.template import Context, loader, RequestContext
 from django.shortcuts import render_to_response
 from django.http import HttpResponse, HttpResponseRedirect
-from reply.models import Person, Reply, FailedAttempt
+from reply.models import Person, Reply, FailedAttempt, InviteEmail
 
 def index(request):
     attending = Reply.objects.filter(attending=True).all()
@@ -25,12 +25,50 @@ def index(request):
         
         
     return render_to_response('summary.html', {
-        'replyYes' : attending,
-        'replyNo'  : notAttending,
-        'replyNone' : noReply,
+        'replyYes' : sorted(attending, key=lambda reply: reply.summaryText),
+        'replyNo'  : sorted(attending, key=lambda reply: reply.summaryText),
+        'replyNone' : sorted(noReply, key=lambda reply: reply.summaryText),
         'numAttending' : numAttending,
         'numNotAttending' : numNotAttending,
         'numNotReplied' : numNotReplied,
+    }, context_instance=RequestContext(request))
+
+def emails(request):
+    replies = Reply.objects.filter().all()
+    for reply in replies:
+        reply.summaryText = reply._attendeeName()
+        reply.emailString = ", ".join((r.email for r in reply.emails.all()))
+    
+    return render_to_response('emails.html', {
+        'replies' : sorted(replies, key=lambda reply: len(reply.emails.all())),
+    }, context_instance=RequestContext(request))
+    
+def updateemails(request):
+    replyID = request.POST.get('replyid')
+    reply = None
+    try:
+        reply = Reply.objects.get(pk=request.POST.get('replyid'))
+    except:
+        pass
+    
+    if reply is None:
+        return render_to_response('emailUpdate.html', {
+            'error' : "Error updating person: not found",
+        }, context_instance=RequestContext(request))
+        
+    
+    emails = request.POST.get('emails')
+    for email in reply.emails.all():
+        email.delete()
+    for email in emails.split(','):
+        e = InviteEmail()
+        e.email = email.strip()
+        e.save()
+        reply.emails.add(e)
+        
+    reply.save()
+    return render_to_response('emailUpdate.html', {
+        'replyName' : reply._attendeeName(),
     }, context_instance=RequestContext(request))
     
 def newreply(request):
@@ -60,7 +98,12 @@ def createupdate(request):
             p.save()
             r.invitedPeople.add(p)
             r.attendingPeople.add(p)
-        r.email = request.POST.get('email')
+        emails = request.POST.get('email')
+        for email in emails.split(','):
+            e = InviteEmail()
+            e.email = email.strip()
+            e.save()
+            reply.emails.add(e)
         r.hasPlusOne = request.POST.get('allowPlusOne') or False
         r.save()
     
